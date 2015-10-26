cd0c505dfa06bb3d606e0084bc536313ffdb1954
Bruno Gaspar <brunofgaspar1@gmail.com>
2015-10-26 21:52:19 +0000

    Add copyright headers to the tests stubs files.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- tests/Stubs/Foo.php
+++ tests/Stubs/Foo.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * Part of the Data Grid Laravel package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Data Grid Laravel
+ * @version    1.0.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2015, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
 namespace Cartalyst\DataGrid\Laravel\Tests\Stubs;
 
 use Cartalyst\Attributes\EntityTrait;
